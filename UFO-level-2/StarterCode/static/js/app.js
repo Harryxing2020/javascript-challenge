f033594501d8abb53efa3ec3c045ebbfa66aef3c
harryxing@hotmail.com <harryxing@hotmail.com>
2020-09-15 18:00:16 -0700

    10th commit

--- UFO-level-2/StarterCode/static/js/app.js
+++ UFO-level-2/StarterCode/static/js/app.js
@@ -1,24 +1,36 @@
 // from data.js
 var tableData = data;
-
-// YOUR CODE HERE!
-
 var tbody = d3.select("tbody");
 var table = d3.select("table");
 tbody.style("text-align", "center")
-
-tableData.forEach(function (ufodata) {
-    var row = tbody.append("tr")
-    Object.entries(ufodata).forEach(function ([key, value]) {
-        var cell = row.append("td")
-        cell.text(value)
+/////////////////////////////////////////////////////////////////////
+//Display tables
+function displayData(showData) {
+    // clear data 
+    tbody.html("");
+    // show the data from json data 
+    showData.forEach(function (ufodata) {
+        var row = tbody.append("tr")
+        Object.entries(ufodata).forEach(function ([key, value]) {
+            var cell = row.append("td")
+            cell.text(value)
+        })
     })
-})
+}
 
+/////////////////////////////////////////////////////////////////////
+// show all ufo data in dataset
+displayData(tableData)
+/////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////
+// show datatime search ufo data in dataset
+/////////////////////////////////////////////////////////////////////
+
+// Select the button
 var buttonDatetime = d3.select("#filter-btn-datetime");
 // Select the form
 var formDatetime = d3.select("#ufoform-datetime");
-
 // Create event handlers 
 buttonDatetime.on("click", runEnterDatetime);
 formDatetime.on("submit", runEnterDatetime);
@@ -26,31 +38,22 @@ formDatetime.on("submit", runEnterDatetime);
 // Complete the event handler function for the form
 function runEnterDatetime() {
 
-    console.log("-----------------------------------")
-
     // Prevent the page from refreshing
     d3.event.preventDefault();
     // Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
     // Get the value property of the input element
     var inputValue = inputElement.property("value");
-    console.log(inputValue);
-
     var filteredDatas = tableData.filter(ufodata => ufodata.datetime === inputValue);
-    console.log(filteredDatas);
 
-    ///////////////////////////////////////////////////////////////////////////////
-    tbody.html("");
-    filteredDatas.forEach(function (filteredData) {
-        var row = tbody.append("tr")
-        Object.entries(filteredData).forEach(function ([key, value]) {
-            var cell = row.append("td")
-            cell.text(value)
-        })
-    })
-    /////////////////////////////////////////////////////////////////////////////////
+    if (filteredDatas.length===0){
+        // cannot find any data 
+        // show all data
+        filteredDatas = tableData
+    }
+    // show data 
 
-
+    displayData(filteredDatas)
 };
 
 
